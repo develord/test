9bb155d5d5eca808b8a7ebbd3bb9f40d6c9169ce
mohamed amine fredj <mohamed.amine.fredj@gmail.com>
2020-07-12 23:15:30 +0100

    fix

--- pages/ad-admin/index.vue
+++ pages/ad-admin/index.vue
@@ -39,9 +39,9 @@
   </v-main>
 </template>
 <script>
-import Mapchart from '~/componants/Mapchart.vue'
-import Comparisationchart from '~/componants/Comparisationchart.vue'
-import AccountList from '~/componants/AccountList.vue'
+import Mapchart from '@/componants/Mapchart.vue'
+import Comparisationchart from '@/componants/Comparisationchart.vue'
+import AccountList from '@/componants/AccountList.vue'
 import { EventBus } from '~/plugins/event-bus'
 
 export default {
