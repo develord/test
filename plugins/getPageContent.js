b86305a62610f9bc97e5c6da3bbf709835db2e60
mohamed amine fredj <mohamed.amine.fredj@gmail.com>
2020-11-28 20:46:09 +0100

    fix 1.2

--- plugins/getPageContent.js
+++ plugins/getPageContent.js
@@ -3,7 +3,11 @@ export default ({ app }, inject) => {
   inject('getPageContent', async (settings, { params, route, app, error, store, payload, $payloadURL, redirect }) => {
     const res = await store.dispatch('findPage', route.fullPath)
     // if (res.data.page && res.data.page.componentName) {
-    const component = res.data?.page?.componentName
+    // const component = res.data?.page?.componentName do not work for older node version
+    let component
+    if (res && res.data && res.data.page) {
+      component = res.data?.page?.componentName
+    }
     const config = (await import('~/components/front/schemas.json')).default
     const pageName = component || route.name
     if (pageName in config) {
