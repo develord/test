d04cda7d5039f63a47aed994655f9c8ae11e4dbb
aminefr <mohamed.amine.fredj@gmail.com>
2021-01-02 22:43:47 +0100

    fix host

--- server/config/env.js
+++ server/config/env.js
@@ -2,12 +2,12 @@
 const dotenv = require('dotenv')
 const config = require('../../nuxt.config')
 
-config.dev = process.env.NODE_ENV !== 'production'
+config.dev = process.env.NODE_ENV === 'development'
 
 const path = config.dev
   ? '.env.local'
   : '.env'
-
+console.log(path)
 dotenv.config({ path })
 
 module.exports = {
