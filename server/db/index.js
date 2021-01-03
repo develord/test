d7fb9dabaf0c6ac26a11a054cb0cd4932f1328c9
aminefr <mohamed.amine.fredj@gmail.com>
2021-01-03 20:00:03 +0100

    add images

--- server/db/index.js
+++ server/db/index.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose')
 
 module.exports = async () => {
   try {
+    console.log(process.env.DATABASE_URL)
     await mongoose.connect(
       process.env.DATABASE_URL,
       { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false },
