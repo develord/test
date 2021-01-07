c814ca701c68808e6785d4b3cdda770ce0e094da
aminefr <mohamed.amine.fredj@gmail.com>
2021-01-07 15:27:23 +0100

    fix mp2 config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,11 +3,12 @@ module.exports = {
     {
       name: 'charma',
       exec_mode: 'cluster',
-      instances: 'max', // Or a number of instances
-      script: './node_modules/nuxt/bin/nuxt.js',
-      args: 'start',
+      instances: '1', // Or a number of instances
+      script: './server/index.js',
+      args: '',
       env: {
-        'NODE_ENV': 'production'
+        'NODE_ENV': 'production',
+        'NUXT_TELEMETRY_DISABLED': 1
       }
     }
   ]
