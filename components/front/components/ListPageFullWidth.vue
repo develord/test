4dbdec40227ebc0efa3844483c2587da2f105829
aminefr <mohamed.amine.fredj@gmail.com>
2021-02-12 01:08:07 +0100

    fix date

--- components/front/components/ListPageFullWidth.vue
+++ components/front/components/ListPageFullWidth.vue
@@ -57,7 +57,7 @@
             <p class="article-desc">
               {{ post.journal }} ,
               <template v-if=" post.published">
-                {{ parseInt(post.published.substring(0,4)) + 1 }}
+                {{ parseInt(post.published.substring(0,4)) }}
               </template>
             </p>
             <div class="tags-box">
@@ -112,7 +112,7 @@ export default {
           this.listPost = this.listPublication
         } else {
           this.listPost = this.listPublication.filter(el => el.tags.filter(tag =>
-            (parseInt(this.$route.query.req) === parseInt(el.published.substring(0, 4)) + 1 ? parseInt(el.published.substring(0, 4)) + 1 : tag.name) === newVal
+            (parseInt(this.$route.query.req) === parseInt(el.published.substring(0, 4)) ? parseInt(el.published.substring(0, 4)) : tag.name) === newVal
           ).length > 0)
         }
       }
