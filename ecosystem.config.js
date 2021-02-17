9b84ed63dde369a086f0f8baf77e0a482bd3399f
aminefr <mohamed.amine.fredj@gmail.com>
2021-02-18 00:45:32 +0100

    ff

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,7 +5,7 @@ module.exports = {
       exec_mode: 'cluster',
       instances: '1', // Or a number of instances
       script: './node_modules/nuxt/bin/nuxt.js',
-      args: 'start:modern',
+      args: 'start',
       env: {
         'NODE_ENV': 'production',
         'NUXT_TELEMETRY_DISABLED': 1
